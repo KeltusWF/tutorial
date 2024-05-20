e77dbc48ee54b47ed341f12ca7c2f201b5c40c2c
KeltusWF <aitimir200323@gmail.com>
2024-05-20 17:24:53 +0900

    Add files via upload

---
+++ resolve1.py.py
@@ -0,0 +1,37 @@
+#                                  задание 1
+print(len('priwet'))
+print(len('priwet' + 'kak dela?'))
+print(len('priwet' + ' ' + 'kak dela?'))
+print('priwet' + ' ' + 'kak dela?')
+#программирование это весело :3
+print(len('kak dela?'))
+
+
+
+#                                  задание 2
+first, second = 4, 5
+summa = first + second
+diff = first - second
+print(summa, diff)
+
+
+
+
+#                                  задание 3
+first, second, third = 10, 15, 20
+mean=(first + second + third)/3
+print(mean)
+
+
+
+#                                  задание 4
+first_string = 'Вторник'
+second_string = 'Понедельник'
+print(second_string, first_string, sep=', ')
+
+
+
+#                                  задание 5
+a, b, c = 2, 3, 5
+f = (a*b) + (a*c)
+print(f**3/2)
